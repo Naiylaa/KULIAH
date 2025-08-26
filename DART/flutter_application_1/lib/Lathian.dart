51fa7e35e7fa2f48ac0cbdf42b21a7d8aba4e5df
Naiyla S. Karim <naylaskarim@gmail.com>
2025-08-26 11:44:13 +0900

    Biodata

---
+++ DART/flutter_application_1/lib/Lathian.dart
@@ -0,0 +1,16 @@
+void main() {
+  String name = "Naiyla S. Karim" ;
+  int umur = 20;
+  double berat = 46;
+  num tinggi = 158.7;
+  dynamic prodi = "informatika";
+  bool isActive = false ;
+
+  print('Nama : $name');
+  print('Umur : $umur');
+  print('Berat badan : $berat');
+  print('Tinggi badan : $tinggi');
+  print('Prodi : $prodi');
+  print('Mahasiswa ini statusnya : $isActive');
+
+}
